3d93e5122bb3f677822dd1aa9acff933cffce35c
Dmytro Azarnyi <dazarny@gmail.com>
2020-11-11 16:46:31 +0200

    Add project templates

---
+++ seriallib.cpp
@@ -0,0 +1,10 @@
+﻿// seriallib.cpp : Определяет функции для статической библиотеки.
+//
+
+#include "pch.h"
+#include "framework.h"
+
+// TODO: Это пример библиотечной функции.
+void fnseriallib()
+{
+}
